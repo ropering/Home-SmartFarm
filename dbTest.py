769b48b2dbcf489f6dd5fd4072e9f1d49005e3ba
Myeong Gyun O <50795314+ropering@users.noreply.github.com>
2021-05-20 10:50:32 +0900

    센서값 - db - db - html 출력
    (온도, 습도, 수위 값, 토양수분 값) ---> db 저장 --->  db ---> html에 db값 출력 성공

--- dbTest.py
+++ dbTest.py
@@ -1,6 +1,8 @@
 '''
-값 db 삽입 - db에서 값 가져와서 출력
+21.05.19
+(온도, 습도, 수위 값, 토양수분 값) ---> db 저장 --->  db ---> html에 db값 출력 성공
 '''
+
 #스레딩 라이브러리
 import threading
 #온습도 라이브러리 #포트 4번
@@ -12,29 +14,34 @@ import sqlite3 as sql
 import RPi.GPIO as GPIO
 import spidev
 
+dbName = 'database.db'
 
-#물 수위 입력
-global vontage
+app = Flask(__name__)
 
+@app.route('/')                   #기본주소('/')로 들어오면
+def home():
+    while(True) :
+        # 센서에서 값 받아오기
+        soilValue = read_spi_adc(1) ## MCP3008포트, 원래 0 / 지금은 포트 1
+        waterValue = round((readAnalog(WATER_CHANNEL) * 3.3/1024) * 100, 1) # *100 은 일부러 값을 높인 것
+        tem, hum = getDHTdata()
 
-#토양 수분 입력
-def read_spi_adc(adcChannel):
-    adcValue = 0
-    buff = spi.xfer2([1, (8+adcChannel) << 4, 0])
-    adcValue = ((buff[1] & 3) << 8)+buff[2]
-    return (adcValue/10)
+        try:
+            with sql.connect(dbName) as con :
+                cur = con.cursor()
+            # 데이터 db에 삽입  
+            cur.execute('INSERT INTO STUDENTS (NAME, ADDR, CITY, PIN) VALUES (?,?,?,?)', (tem, hum, waterValue, soilValue))
+            con.commit() 
+            sleep(1)
+            # DB 데이터 가져오기
+            cur.execute('SELECT * FROM STUDENTS ORDER BY ROWID DESC LIMIT 1') #마지막 행 
+            row = cur.fetchone()
+            sleep(1)
+            return render_template('index_04.html',  temp = row[0], hum = row[1], water = row[2], soil = row[3]) #index.html에 전체 led현황을 함께 전달 
+        finally:
+            cur.close()
 
-adcValue = read_spi_adc(1) ## MCP3008포트, 원래 0 / 지금은 포트 1
-# print("토양수분: %d "%(adcValue))
-digit_val=GPIO.input(DIGIT)
-# print("Digit Value : %d"%(digit_val))
-    
-while(True):
-    voltage = (readAnalog(WATER_CHANNEL) * 3.3/1024) * 100
-    tem, hum = getDHTdata()
-    logDB(tem, hum, voltage, adcValue)
-    print(returnDB())
-    sleep(2)
-
+if __name__ == '__main__':
+    app.run(debug=True, port=80, host='0.0.0.0')
 
 
